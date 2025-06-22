bfd73724e9fb8109ce2a917a790c62f1e9fb7047
JindotNyang <pgwoon77@gmail.com>
2025-06-22 22:14:27 +0900

    Add subpages to index.html

---
+++ docs/script.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', () => {
+  const hamburgerMenu = document.getElementById('hamburger_menu');
+  const sideMenu = document.getElementById('side_menu');
+  const closeBtn = document.getElementById('close_btn');
+
+  hamburgerMenu.addEventListener('click', () => {
+    sideMenu.classList.add('open');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('open');
+  });
+
+  const videoCnt = document.getElementById('video_cnt');
+  const isMobile = window.innerWidth <= 768;
+
+  const videoPath = isMobile
+    ? './videos/Gomniverse Mobile.mp4'
+    : './videos/Gomniverse Intro.mp4';
+
+  videoCnt.innerHTML = `
+    <video autoplay muted loop playsinline>
+      <source src="${videoPath}" type="video/mp4">
+    </video>
+  `;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileCnt = document.getElementById('mobile_cnt');
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 768) {
+    mobileCnt.style.display = 'block';
+  } else {
+    mobileCnt.style.display = 'none';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerBtn = document.querySelector('#mobile_cnt .circle'); // 가운데 버튼
+  const toggleMenu = document.getElementById('toggle_menu');
+
+  hamburgerBtn.addEventListener('click', () => {
+    toggleMenu.classList.toggle('show');
+  });
+});
+
+toggleButton.classList.toggle('rotate');
+
+
+
